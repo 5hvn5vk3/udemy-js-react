ec7f583dc15ccf4d92f37d12bca2c9517d0c6985
5hvn5vk3 <kusami0907@yahoo.co.jp>
2025-08-15 08:58:28 +0900

    propsの勉強（分割代入の活用方法、書き方）

--- udemy-react/src/components/ColorfulMessage.jsx
+++ udemy-react/src/components/ColorfulMessage.jsx
@@ -1,9 +1,9 @@
-export const ColorfulMessage = (props) => {
+export const ColorfulMessage = ({color, children}) => { // 括弧の中で分割代入 {color, children} = props
     const contentStyleA = {
-        color: props.color,
+        color, // 省略記法
         fontSize: "18px",
     };
     return (
-        <p style = {contentStyleA}>{props.children}</p>
+        <p style = {contentStyleA}>{children}</p>
     )
 }
