36f2ba3fd4d5bb2d2de437170ffbaf2345a35e3b
5hvn5vk3 <kusami0907@yahoo.co.jp>
2025-08-15 08:52:54 +0900

    propsの勉強

--- udemy-react/src/App.jsx
+++ udemy-react/src/App.jsx
@@ -2,20 +2,18 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { ColorfulMessage } from "./components/ColorfulMessage";
 
 export const App = () => {
   const onClickButton = () => {
     alert("clicked");
   };
-  const contentStyle = {
-    color: "blue",
-    fontSize: "20px", // font-sizeとは書かない。キャメルケースで書く。JavaScriptのDOM操作の際のclassNameのような書き方に似ている。
-  };
   
   return (
     <>
       <h1 style = {{color: "red"}}>こんにちは！</h1>
-      <p style = {contentStyle}>お元気ですか？</p>
+      <ColorfulMessage color = "blue">お元気ですか？</ColorfulMessage>
+      <ColorfulMessage color = "green">元気です！</ColorfulMessage>
       <button onClick = {onClickButton}>Click me</button>
     </>
   );  
---
+++ udemy-react/src/components/ColorfulMessage.jsx
@@ -0,0 +1,9 @@
+export const ColorfulMessage = (props) => {
+    const contentStyleA = {
+        color: props.color,
+        fontSize: "18px",
+    };
+    return (
+        <p style = {contentStyleA}>{props.children}</p>
+    )
+}
