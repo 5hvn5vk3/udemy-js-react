cd69cad9084c9ef27c76a8ec0d3abb6b06e53504
5hvn5vk3 <kusami0907@yahoo.co.jp>
2025-08-15 18:00:43 +0900

    再レンダリングについて

--- udemy-react/src/App.jsx
+++ udemy-react/src/App.jsx
@@ -4,12 +4,21 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import { ColorfulMessage } from "./components/ColorfulMessage";
 
+
+
 export const App = () => {
+  console.log("--App--"); // 開発時（StrictMode）はレンダリングが2回走る
   const [num, setNum] = useState(0); // フックは一番上でしか呼べない
+  const [isSHowFace, setIsSHowFace] = useState(true);
+
   const onClickCountUp = () => {
     setNum((prev) => prev + 1); // 関数を渡すと今の引数の値が設定できる。次の行と合わせて2ずつ増えていくような動きをさせることができる。
     setNum((prev) => prev + 1);
   };
+
+  const onClickToggle = () => {
+    setIsSHowFace(!isSHowFace);
+  }
   return (
     <>
       <h1 style = {{color: "red"}}>こんにちは！</h1>
@@ -17,6 +26,9 @@ export const App = () => {
       <ColorfulMessage color = "green">元気です！</ColorfulMessage>
       <button onClick = {onClickCountUp}>Click me</button>
       <p>{num}</p>
+
+      <button onClick ={onClickToggle}>on/off</button>
+      {isSHowFace && <p>（´・ω・`）</p>}
     </>
   );  
 };
