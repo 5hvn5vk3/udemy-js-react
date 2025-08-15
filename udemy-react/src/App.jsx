57d04c9ba10ade7b99bc1dbd74de278224e451f9
5hvn5vk3 <kusami0907@yahoo.co.jp>
2025-08-15 17:28:35 +0900

    Stateの勉強

--- udemy-react/src/App.jsx
+++ udemy-react/src/App.jsx
@@ -5,16 +5,17 @@ import "./App.css";
 import { ColorfulMessage } from "./components/ColorfulMessage";
 
 export const App = () => {
-  const onClickButton = () => {
-    alert("clicked");
+  const [num, setNum] = useState(0);
+  const onClickCountUp = () => {
+    setNum(num + 1);
   };
-  
   return (
     <>
       <h1 style = {{color: "red"}}>こんにちは！</h1>
       <ColorfulMessage color = "blue">お元気ですか？</ColorfulMessage>
       <ColorfulMessage color = "green">元気です！</ColorfulMessage>
-      <button onClick = {onClickButton}>Click me</button>
+      <button onClick = {onClickCountUp}>Click me</button>
+      <p>{num}</p>
     </>
   );  
 };
