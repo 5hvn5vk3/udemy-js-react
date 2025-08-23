c96c961d33c6a9dcd2d4f4828c8e0077442186b4
5hvn5vk3 <kusami0907@yahoo.co.jp>
2025-08-23 11:13:12 +0900

    Reactを使ったTODOアプリの制作を開始します

---
+++ udemy-react/src/Todo.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./Todo.css";
+import { ColorfulMessage } from "./components/ColorfulMessage";
+
+
+
+export const Todo = () => {
+
+  return (
+    <>
+      <div>
+        <input placeholder=""></input>
+        <button>追加</button>
+      </div>
+
+      <div>
+        <p>未完了のTODO</p>
+      </div>
+
+      <div>
+        <ul>
+          <li>
+            <p>TODOです</p>
+            <button>完了</button>
+            <button>削除</button>
+          </li>
+          <li>
+            <p>TODOです</p>
+            <button>完了</button>
+            <button>削除</button>
+          </li>
+        </ul>
+      </div>
+    </>
+  );  
+};
