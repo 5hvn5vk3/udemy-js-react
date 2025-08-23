4ca3a6b0e95e5bdef522949789a1d2743d0d49fa
5hvn5vk3 <kusami0907@yahoo.co.jp>
2025-08-23 17:35:31 +0900

    タスクの削除機能の追加とモックの修正
    - タスクの削除機能を追加した
    - complete-area が incomplete-area と同じになってしまっていたので、ついでにそちらを修正した

--- udemy-react/src/Todo.jsx
+++ udemy-react/src/Todo.jsx
@@ -19,24 +19,29 @@ export const Todo = () => {
     setIncompleteTodos(newTodos);
     setTodoText("");
   };
+  const onClickDelete = (index) => {
+    const newTodos = [...incompleteTodos]; // incompleteTodos自体に処理をしてしまうと、更新を判定してもらえない。Reactのセット関数は配列が新しいものになったかどうかで判定するため
+    newTodos.splice(index, 1);
+    setIncompleteTodos(newTodos);
+  };
 
   return (
     <>
       <div className="input-area">
-        <input placeholder="TODOを入力" value={todoText} onChange = {onChangeTodoText}></input>
-        <button onClick = {onClickAdd}>追加</button>
+        <input placeholder="TODOを入力" value={todoText} onChange={onChangeTodoText}></input>
+        <button onClick={onClickAdd}>追加</button>
       </div>
 
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
         <ul>
-          {incompleteTodos.map((todo) => (
+          {incompleteTodos.map((todo, index) => (
             
             <li key={todo}> {/*今回は簡易的にtodoをkeyにしているが本当は一意的なIDがあるのが望ましい*/}
               <div className="list-row">
                 <p className="todo-item">{todo}</p>
                 <button>完了</button>
-                <button>削除</button>
+                <button onClick={() => onClickDelete(index)}>削除</button> {/*単に onClickDelete(index) とだけ書くと読み込み時に毎回それが実行されてしまう*/}
               </div>
             </li>
           ))}
@@ -46,12 +51,11 @@ export const Todo = () => {
       <div className="complete-area">
         <p className="title">完了のTODO</p>
         <ul>
-          {completeTodos.map((todo) => (
+          {completeTodos.map((todo, index) => (
             <li key={todo}>
               <div className="list-row">
                 <p className="todo-item">{todo}</p>
-                <button>完了</button>
-                <button>削除</button>
+                <button>戻す</button>
               </div>
             </li>
           ))}
