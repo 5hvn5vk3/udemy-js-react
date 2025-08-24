33d8129e6419a2fcb5870bfb88a7085b8352effe
5hvn5vk3 <kusami0907@yahoo.co.jp>
2025-08-24 12:02:45 +0900

    コンポーネント化が一旦終わりました

--- udemy-react/src/Todo.jsx
+++ udemy-react/src/Todo.jsx
@@ -3,6 +3,9 @@ import { useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./Todo.css";
+import { InputTodo } from "./components/InputTodo";
+import { IncompleteTodos } from "./components/IncompleteTodos";
+import {CompleteTodos} from "./components/CompleteTodos";
 
 
 
@@ -44,40 +47,20 @@ export const Todo = () => {
 
   return (
     <>
-      <div className="input-area">
-        <input placeholder="TODOを入力" value={todoText} onChange={onChangeTodoText}></input>
-        <button onClick={onClickAdd}>追加</button>
-      </div>
-
-      <div className="incomplete-area">
-        <p className="title">未完了のTODO</p>
-        <ul>
-          {incompleteTodos.map((todo, index) => (
-            
-            <li key={todo}> {/*今回は簡易的にtodoをkeyにしているが本当は一意的なIDがあるのが望ましい*/}
-              <div className="list-row">
-                <p className="todo-item">{todo}</p>
-                <button onClick={() => onClickComplete(index)}>完了</button>
-                <button onClick={() => onClickDelete(index)}>削除</button> {/*単に onClickDelete(index) とだけ書くと読み込み時に毎回それが実行されてしまう*/}
-              </div>
-            </li>
-          ))}
-        </ul>
-      </div>
-
-      <div className="complete-area">
-        <p className="title">完了のTODO</p>
-        <ul>
-          {completeTodos.map((todo, index) => (
-            <li key={todo}>
-              <div className="list-row">
-                <p className="todo-item">{todo}</p>
-                <button onClick={() => onClickBack(index)}>戻す</button>
-              </div>
-            </li>
-          ))}
-        </ul>
-      </div>
+      <InputTodo
+        todoText={todoText}
+        onChange={onChangeTodoText}
+        onClick={onClickAdd}
+      />
+      <IncompleteTodos
+        todos={incompleteTodos}
+        onClickComplete={onClickComplete}
+        onClickDelete={onClickDelete}
+      />
+      <CompleteTodos
+        todos={completeTodos}
+        onClickBack={onClickBack}
+      />
     </>
   );  
 };
