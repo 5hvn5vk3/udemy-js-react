0ca204c08348c594109a56fccc58033802b2dac5
5hvn5vk3 <kusami0907@yahoo.co.jp>
2025-08-24 13:20:53 +0900

    とりあえず上限設定はできた

--- udemy-react/src/Todo.jsx
+++ udemy-react/src/Todo.jsx
@@ -51,11 +51,15 @@ export const Todo = () => {
         todoText={todoText}
         onChange={onChangeTodoText}
         onClick={onClickAdd}
+        disabled={incompleteTodos.length >= 5}
       />
 
-      <p style={{color: "red"}}>
-        登録できる TODO は5個までです。消化してください。
-      </p>
+      {incompleteTodos.length >= 5 &&(
+        <p style={{color: "red"}}>
+          登録できる TODO は5個までです。消化してください。
+        </p>
+      )}
+      
 
       <IncompleteTodos
         todos={incompleteTodos}
--- udemy-react/src/components/InputTodo.jsx
+++ udemy-react/src/components/InputTodo.jsx
@@ -1,6 +1,6 @@
 
 export const InputTodo = (props) => {
-    const {todoText, onChange, onClick} = props;
+    const {todoText, onChange, onClick, disabled} = props;
     const style = {
         backgroundColor: "#c6e5d9",
         width: "400px",
@@ -11,8 +11,8 @@ export const InputTodo = (props) => {
     };
     return (
     <div style={style}>
-        <input placeholder="TODOを入力" value={todoText} onChange={onChange}></input>
-        <button onClick={onClick}>追加</button>
+        <input disabled={disabled} placeholder="TODOを入力" value={todoText} onChange={onChange}></input>
+        <button disabled={disabled} onClick={onClick}>追加</button>
     </div>
     );
 };
